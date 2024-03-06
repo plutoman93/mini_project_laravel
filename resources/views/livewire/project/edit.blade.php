dd6f6ddbda831484f3451e2ecb5812f532b5341a
plutoman93 <anucha.se@rmuti.ac.th>
2024-03-06 11:16:54 +0700

    แก้ข้อความด้านบนฟอร์ม

--- resources/views/livewire/project/edit.blade.php
+++ resources/views/livewire/project/edit.blade.php
@@ -4,7 +4,7 @@
           <div class="container-fluid">
             <div class="row mb-2">
               <div class="col-sm-6">
-                <h1>Project Add</h1>
+                <h1>Project Edit</h1>
               </div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
